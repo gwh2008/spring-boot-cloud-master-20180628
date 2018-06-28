78ae1dbed489eb040cde00623bf846dfe873122c
gaowenhui <gwh_2014@163.com>
2018-06-28 17:39:48 +0800

    gaowh20180628commit-1

---
+++ zipkin/src/main/java/com/vcyber/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package com.vcyber.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+/**
+ * @author gaowenhui
+ */
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}
